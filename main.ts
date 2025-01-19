ab956a89dace7001ea95335c76ebb99130aa626f
Zoltan Komives <zolaemil@users.noreply.github.com>
2025-01-19 16:43:20 +0000

    Increase min duty to 300

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    duty = Math.min(duty + 1, 5)
-    pins.analogWritePin(AnalogPin.P1, duty * 200)
+    duty = Math.min(duty + 1, 4)
+    pins.analogWritePin(AnalogPin.P1, duty * 300)
     basic.showNumber(duty)
 })
 input.onButtonPressed(Button.AB, function () {
@@ -10,9 +10,10 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     duty = Math.max(duty - 1, 0)
-    pins.analogWritePin(AnalogPin.P1, duty * 200)
+    pins.analogWritePin(AnalogPin.P1, duty * 300)
     basic.showNumber(duty)
 })
 let duty = 0
 duty = 0
 basic.showNumber(duty)
+basic.showIcon(IconNames.Skull)
